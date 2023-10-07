e680d00dc8c473ab85dc3288bdb41f15296fb97f
Tokumura <anton.tereshchenko.0003@bk.ru>
2023-10-07 10:24:18 +0300

    add firestore

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,14 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Method to handle user sign in using email and password
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
       UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
+      _firestore.collection("users").doc(result.user!.uid).set(
+          {"uid": result.user!.uid, "email": result.user!.email},
+          SetOptions(merge: true));
+
       User? user = result.user;
       return user;
     } catch (e) {
@@ -22,6 +28,10 @@ class AuthService extends ChangeNotifier {
     try {
       UserCredential result = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+      _firestore
+          .collection("users")
+          .doc(result.user!.uid)
+          .set({"uid": result.user!.uid, "email": result.user!.email});
       User? user = result.user;
       return user;
     } catch (e) {
